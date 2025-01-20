f77d1274157ba3dde47a65afa6e96b60461a351d
João Victor Zilio Verzoto <verzoto@gmail.com>
2025-01-20 14:08:43 -0300

    start

---
+++ guardian/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+# Create your views here.
+def index(request):
+	template = loader.get_template("guardian/index.html")
+	return render(request, "guardian/index.html")
