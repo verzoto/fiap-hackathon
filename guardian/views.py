5c5b47fdb4aa0f2b2f367a655421df36b96c4e69
João Victor Zilio Verzoto <verzoto@gmail.com>
2025-01-20 15:38:55 -0300

    upload and response video files

--- guardian/views.py
+++ guardian/views.py
@@ -1,8 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from django.shortcuts import render
 from django.template import loader
+import cv2
+import tempfile
 
 # Create your views here.
 def index(request):
-	template = loader.get_template("guardian/index.html")
 	return render(request, "guardian/index.html")
+
+
+def upload_video(request):
+	if (request.method == "POST"):
+		file = request.FILES["file"]
+		
+		with tempfile.NamedTemporaryFile() as temp:
+			temp.write(file.read())
+   
+			# Handle the file upload
+			cap = cv2.VideoCapture(temp.name)
+			print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+			print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+			# Check if the file is a video
+			if not cap.isOpened():
+				return HttpResponse("File is not a video")
+			
+			return FileResponse(open(temp.name, "rb"))
